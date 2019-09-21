0c7712f4255ce6075aee70c60c75abc8336be0c9
leonardremigious <leonardremegious@gmail.com>
2019-09-21 20:10:22 +0530

    stack completed

---
+++ src/sample/stack/Stack.java
@@ -0,0 +1,9 @@
+import java.util.List;
+
+public class Stack {
+    Stack next;
+    Integer value;
+     Stack(Integer value){
+        this.value=value;
+     }
+}
---
+++ src/sample/stack/Stackrunner.java
@@ -0,0 +1,13 @@
+public class Stackrunner {
+    public static void main(String args[]){
+        StackImplementation run=new StackImplementation();
+        run.push(3);
+        run.push(4);
+        run.push(5);
+        System.out.println(run.elementAt(2));
+
+        run.pop();
+        System.out.println(run.elementAt(0));
+        System.out.println(run.elementAt(2));
+    }
+}
